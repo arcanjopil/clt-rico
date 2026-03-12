6f75a225a90f2fce42c222f6ad0de77d0349d6d8
Seu Nome Aqui <arcanjopil@gmail.com>
2026-03-12 00:03:38 -0300

    fix: force service worker update

--- app/SWRegister.js
+++ app/SWRegister.js
@@ -8,10 +8,26 @@ export default function SWRegister() {
       window.addEventListener("load", function () {
         navigator.serviceWorker.register("/sw.js").then(
           function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
+            console.log("Service Worker registered!");
+            
+            // Check for updates
+            registration.onupdatefound = () => {
+              const installingWorker = registration.installing;
+              if (installingWorker) {
+                installingWorker.onstatechange = () => {
+                  if (installingWorker.state === 'installed') {
+                    if (navigator.serviceWorker.controller) {
+                      // New version found! Reload.
+                      console.log('New content available, please refresh.');
+                      window.location.reload();
+                    }
+                  }
+                };
+              }
+            };
+
+            // Force update check
+            registration.update();
           },
           function (err) {
             console.log("Service Worker registration failed: ", err);
